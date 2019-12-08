830c9f9cb44d175d318eb3ee778896f9eb711b1a
Vu Tung Duong <duong.vu@bigin.vn>
2019-12-08 18:06:18 +0700

    install material theme

---
+++ src/app/template/foot-bar/foot-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'template-foot-bar',
+  templateUrl: './foot-bar.component.html',
+  styleUrls: ['./foot-bar.component.css']
+})
+export class FootBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/template/template.module.ts
+++ src/app/template/template.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
-
-
+import { HomePageComponent } from "./home-page/home-page.component";
+import { NavBarComponent } from "./nav-bar/nav-bar.component";
+import { ContentComponent } from "./content/content.component";
+import { FootBarComponent } from "./foot-bar/foot-bar.component";
 
 @NgModule({
-	declarations: [],
-	imports: [
-		CommonModule
-	]
+	declarations: [
+		HomePageComponent,
+		NavBarComponent,
+		ContentComponent,
+		FootBarComponent
+	],
+	imports: [CommonModule],
+	exports: [HomePageComponent]
 })
-export class TemplateModule { }
+export class TemplateModule {}
