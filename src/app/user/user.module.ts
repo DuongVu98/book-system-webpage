85bd6988fba5e0046a4c7c262826b4ca3cb55032
Vu Tung Duong <duong.vu@bigin.vn>
2019-12-23 16:16:53 +0700

    add components to profiles

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -6,9 +6,12 @@ import { RouterModule } from "@angular/router";
 import { userRoutes } from "./user.routes";
 import { LoginComponent } from "./login/login.component";
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { UserBooksComponent } from './user-books/user-books.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { UserSettingsComponent } from './user-settings/user-settings.component';
 
 @NgModule({
-	declarations: [UserDashboardComponent, LoginComponent],
+	declarations: [UserDashboardComponent, LoginComponent, UserBooksComponent, UserProfileComponent, UserSettingsComponent],
 	imports: [CommonModule, MaterialModule, RouterModule.forChild(userRoutes), MDBBootstrapModule],
 	exports: [UserDashboardComponent]
 })
--- src/app/user/user.routes.ts
+++ src/app/user/user.routes.ts
@@ -1,8 +1,20 @@
 import { Routes } from "@angular/router";
 import { UserDashboardComponent } from "./user-dashboard/user-dashboard.component";
 import { LoginComponent } from "./login/login.component";
+import { UserBooksComponent } from "./user-books/user-books.component";
+import { UserProfileComponent } from "./user-profile/user-profile.component";
+import { UserSettingsComponent } from "./user-settings/user-settings.component";
 
 export const userRoutes: Routes = [
-	{ path: "profile", component: UserDashboardComponent },
-	{ path: "login", component: LoginComponent }
+	{ path: "login", component: LoginComponent },
+	{
+		path: "profile",
+		component: UserDashboardComponent,
+		children: [
+			{ path: "", redirectTo: "profile", pathMatch: "full" },
+			{ path: "user-profile", component: UserProfileComponent },
+			{ path: "user-books", component: UserBooksComponent },
+			{ path: "user-settings", component: UserSettingsComponent }
+		]
+	}
 ];
