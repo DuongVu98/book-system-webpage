ff1ea90296f21591fbd7e59d7c5e011ac8b2873a
Vu Tung Duong <duong.vu@bigin.vn>
2019-12-24 12:25:16 +0700

    login component

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from "@angular/core";
+import { UserAuthenticationService } from "src/app/service/user-authentication.service";
+import { Router } from "@angular/router";
 
 @Component({
 	selector: "user-login",
@@ -6,6 +8,31 @@ import { Component, OnInit } from "@angular/core";
 	styleUrls: ["./login.component.css"]
 })
 export class LoginComponent implements OnInit {
-	constructor(){}
+	private loginInput;
+	private loggedUser;
+	private user;
+
+	constructor(
+		private userAuthenticationService: UserAuthenticationService,
+		private router: Router
+	) {
+		this.loggedUser = false;
+		this.loginInput = {
+			username: null,
+			password: null
+		};
+		this.user = null;
+	}
+
 	ngOnInit() {}
+
+	login() {
+		this.user = this.userAuthenticationService.login(this.loginInput);
+
+		if (this.user != null) {
+			this.loggedUser = true;
+		} else {
+			this.loggedUser = false;
+		}
+	}
 }
